aadfe169d22c50672aaa966c5b2c2938f2453284
developerbuilds <prince.malethia.36@gmail.com>
2020-11-27 23:03:00 +0530

    frontend refurbished

--- machine/urls.py
+++ machine/urls.py
@@ -11,9 +11,18 @@ def Index(request):
     }
     return render(request, template_name,context)
 
+def About(request):
+    template_name='about us.html'
+    context = {
+        'aboutusPage':True,
+        'title':'ML MODELLDER | About Us'
+    }
+    return render(request, template_name,context)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',Index),
+    path('about-us/',About),
     path('classification/', include('classification.urls')),
     path('deeplearning/', include('deeplearning.urls')),
     path('regression/', include('regression.urls')),
