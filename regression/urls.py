bedd5889fbd5cd735b214ff0e7a0ef4c5296c67a
developerbuilds <prince.malethia.36@gmail.com>
2020-11-28 12:54:34 +0530

    base extends all

--- regression/urls.py
+++ regression/urls.py
@@ -12,10 +12,10 @@ def Index(request):
     return render(request, template_name,context)
 urlpatterns = [
     path('',Index,name="regression"),
-    path('decisiontree-regression/', DecisonTreeRegression,name="decisiontree-regression"),
+    path('simple-linear-regression/', SimpleLinearRegression,name="simple-linear-regression"),
     path('multiple-linear-regression/', MultipleLinearRegression,name="multiple-linear-regression"),
     path('polynomial-regression/', PolynomialRegression,name="polynomial-regression"),
-    path('random-forest-regression/', RandomForestRegression,name="random-forest-regression"),
-    path('simple-linear-regression/', SimpleLinearRegression,name="simple-linear-regression"),
     path('support-vector-regression/', SupportVectorRegression,name="support-vector-regression"),
+    path('decisiontree-regression/', DecisonTreeRegression,name="decisiontree-regression"),
+    path('random-forest-regression/', RandomForestRegression,name="random-forest-regression"),
 ]
