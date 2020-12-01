40894dd455773913adb4203b090ef8379619a282
developerbuilds <prince.malethia.36@gmail.com>
2020-12-01 14:11:01 +0530

    exception added

--- classification/views.py
+++ classification/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render ,redirect
+from django.http import HttpResponse
 from django.contrib import messages
 import numpy as np
 import matplotlib.pyplot as plt
@@ -27,24 +28,27 @@ def DecisionTree(request):
         if not input_csv_file.name.endswith('.csv'):
             messages.error(request,'input file is not a valid CSV file')
             return redirect('decision-tree')
-        dataset = pd.read_csv(csv_file)
-        lst = pd.read_csv(input_csv_file)
-        X = dataset.iloc[:, :-1].values
-        y = dataset.iloc[:, -1].values
-        imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
-        imputer.fit(X[:, :])
-        X[:, :] = imputer.transform(X[:, :])
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
-        sc = StandardScaler()
-        X_train = sc.fit_transform(X_train)
-        X_test = sc.transform(X_test)
-        classifier = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
-        classifier.fit(X_train, y_train)
-        result = classifier.predict(sc.transform(lst))
-        f_data = []
-        for res in result:
-            f_data.append(res)
-        context['result'] = f_data
+        try:
+            dataset = pd.read_csv(csv_file)
+            lst = pd.read_csv(input_csv_file)
+            X = dataset.iloc[:, :-1].values
+            y = dataset.iloc[:, -1].values
+            imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+            imputer.fit(X[:, :])
+            X[:, :] = imputer.transform(X[:, :])
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+            sc = StandardScaler()
+            X_train = sc.fit_transform(X_train)
+            X_test = sc.transform(X_test)
+            classifier = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
+            classifier.fit(X_train, y_train)
+            result = classifier.predict(sc.transform(lst))
+            f_data = []
+            for res in result:
+                f_data.append(res)
+            context['result'] = f_data
+        except Exception as e:
+            return HttpResponse("Error Occured , Reason : " + str(e))
     return render(request,template_name,context)
 
 def KNearestNeighbors(request):
@@ -61,24 +65,27 @@ def KNearestNeighbors(request):
         if not input_csv_file.name.endswith('.csv'):
             messages.error(request,'input file is not a valid CSV file')
             return redirect('k-nearest-neighbors')
-        dataset = pd.read_csv(csv_file)
-        lst = pd.read_csv(input_csv_file)
-        X = dataset.iloc[:, :-1].values
-        y = dataset.iloc[:, -1].values
-        imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
-        imputer.fit(X[:, :])
-        X[:, :] = imputer.transform(X[:, :])
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
-        sc = StandardScaler()
-        X_train = sc.fit_transform(X_train)
-        X_test = sc.transform(X_test)
-        classifier = KNeighborsClassifier(n_neighbors = 5, metric = 'minkowski', p = 2)
-        classifier.fit(X_train, y_train)
-        result = classifier.predict(sc.transform(lst))
-        f_data = []
-        for res in result:
-            f_data.append(res)
-        context['result'] = f_data
+        try:
+            dataset = pd.read_csv(csv_file)
+            lst = pd.read_csv(input_csv_file)
+            X = dataset.iloc[:, :-1].values
+            y = dataset.iloc[:, -1].values
+            imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+            imputer.fit(X[:, :])
+            X[:, :] = imputer.transform(X[:, :])
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+            sc = StandardScaler()
+            X_train = sc.fit_transform(X_train)
+            X_test = sc.transform(X_test)
+            classifier = KNeighborsClassifier(n_neighbors = 5, metric = 'minkowski', p = 2)
+            classifier.fit(X_train, y_train)
+            result = classifier.predict(sc.transform(lst))
+            f_data = []
+            for res in result:
+                f_data.append(res)
+            context['result'] = f_data
+        except Exception as e:
+            return HttpResponse("Error Occured , Reason : " + str(e))
     return render(request,template_name,context)
 
 
@@ -96,24 +103,27 @@ def KernelSVM(request):
         if not input_csv_file.name.endswith('.csv'):
             messages.error(request,'input file is not a valid CSV file')
             return redirect('kernel-svm')
-        dataset = pd.read_csv(csv_file)
-        lst = pd.read_csv(input_csv_file)
-        X = dataset.iloc[:, :-1].values
-        y = dataset.iloc[:, -1].values
-        imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
-        imputer.fit(X[:, :])
-        X[:, :] = imputer.transform(X[:, :])
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
-        sc = StandardScaler()
-        X_train = sc.fit_transform(X_train)
-        X_test = sc.transform(X_test)
-        classifier = SVC(kernel = 'rbf', random_state = 0)
-        classifier.fit(X_train, y_train)
-        result = classifier.predict(sc.transform(lst))
-        f_data = []
-        for res in result:
-            f_data.append(res)
-        context['result'] = f_data
+        try:
+            dataset = pd.read_csv(csv_file)
+            lst = pd.read_csv(input_csv_file)
+            X = dataset.iloc[:, :-1].values
+            y = dataset.iloc[:, -1].values
+            imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+            imputer.fit(X[:, :])
+            X[:, :] = imputer.transform(X[:, :])
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+            sc = StandardScaler()
+            X_train = sc.fit_transform(X_train)
+            X_test = sc.transform(X_test)
+            classifier = SVC(kernel = 'rbf', random_state = 0)
+            classifier.fit(X_train, y_train)
+            result = classifier.predict(sc.transform(lst))
+            f_data = []
+            for res in result:
+                f_data.append(res)
+            context['result'] = f_data
+        except Exception as e:
+            return HttpResponse("Error Occured , Reason : " + str(e))
     return render(request,template_name,context)
 
 
@@ -131,24 +141,27 @@ def KernelSVM(request):
 #         if not input_csv_file.name.endswith('.csv'):
 #             messages.error(request,'input file is not a valid CSV file')
 #             return redirect('logistic-regression')
-#         dataset = pd.read_csv(csv_file)
-#         lst = pd.read_csv(input_csv_file)
-#         X = dataset.iloc[:, :-1].values
-#         y = dataset.iloc[:, -1].values
-#         imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
-#         imputer.fit(X[:, :])
-#         X[:, :] = imputer.transform(X[:, :])
-#         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
-#         sc = StandardScaler()
-#         X_train = sc.fit_transform(X_train)
-#         X_test = sc.transform(X_test)
-#         classifier = LogisticRegression(random_state = 0)
-#         classifier.fit(X_train, y_train)
-#         result = classifier.predict(sc.transform(lst))
-#         f_data = []
-#         for res in result:
-#             f_data.append(res)
-#         context['result'] = f_data
+#         try:
+    #         dataset = pd.read_csv(csv_file)
+    #         lst = pd.read_csv(input_csv_file)
+    #         X = dataset.iloc[:, :-1].values
+    #         y = dataset.iloc[:, -1].values
+    #         imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+    #         imputer.fit(X[:, :])
+    #         X[:, :] = imputer.transform(X[:, :])
+    #         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+    #         sc = StandardScaler()
+    #         X_train = sc.fit_transform(X_train)
+    #         X_test = sc.transform(X_test)
+    #         classifier = LogisticRegression(random_state = 0)
+    #         classifier.fit(X_train, y_train)
+    #         result = classifier.predict(sc.transform(lst))
+    #         f_data = []
+    #         for res in result:
+    #             f_data.append(res)
+    #          context['result'] = f_data
+    #     except Exception as e:
+    #         return HttpResponse("Error Occured , Reason : " + str(e))
 #     return render(request,template_name,context)
 
 def NaiveBayes(request):
@@ -165,24 +178,27 @@ def NaiveBayes(request):
         if not input_csv_file.name.endswith('.csv'):
             messages.error(request,'input file is not a valid CSV file')
             return redirect('naive-bayes')
-        dataset = pd.read_csv(csv_file)
-        lst = pd.read_csv(input_csv_file)
-        X = dataset.iloc[:, :-1].values
-        y = dataset.iloc[:, -1].values
-        imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
-        imputer.fit(X[:, :])
-        X[:, :] = imputer.transform(X[:, :])
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
-        sc = StandardScaler()
-        X_train = sc.fit_transform(X_train)
-        X_test = sc.transform(X_test)
-        classifier = GaussianNB()
-        classifier.fit(X_train, y_train)
-        result = classifier.predict(sc.transform(lst))
-        f_data = []
-        for res in result:
-            f_data.append(res)
-        context['result'] = f_data
+        try:
+            dataset = pd.read_csv(csv_file)
+            lst = pd.read_csv(input_csv_file)
+            X = dataset.iloc[:, :-1].values
+            y = dataset.iloc[:, -1].values
+            imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+            imputer.fit(X[:, :])
+            X[:, :] = imputer.transform(X[:, :])
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+            sc = StandardScaler()
+            X_train = sc.fit_transform(X_train)
+            X_test = sc.transform(X_test)
+            classifier = GaussianNB()
+            classifier.fit(X_train, y_train)
+            result = classifier.predict(sc.transform(lst))
+            f_data = []
+            for res in result:
+                f_data.append(res)
+            context['result'] = f_data
+        except Exception as e:
+            return HttpResponse("Error Occured , Reason : " + str(e))
     return render(request,template_name,context)
 
 
@@ -200,24 +216,27 @@ def RandomForest(request):
         if not input_csv_file.name.endswith('.csv'):
             messages.error(request,'input file is not a valid CSV file')
             return redirect('random-forest')
-        dataset = pd.read_csv(csv_file)
-        lst = pd.read_csv(input_csv_file)
-        X = dataset.iloc[:, :-1].values
-        y = dataset.iloc[:, -1].values
-        imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
-        imputer.fit(X[:, :])
-        X[:, :] = imputer.transform(X[:, :])
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
-        sc = StandardScaler()
-        X_train = sc.fit_transform(X_train)
-        X_test = sc.transform(X_test)
-        classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0)
-        classifier.fit(X_train, y_train)
-        result = classifier.predict(sc.transform(lst))
-        f_data = []
-        for res in result:
-            f_data.append(res)
-        context['result'] = f_data
+        try:
+            dataset = pd.read_csv(csv_file)
+            lst = pd.read_csv(input_csv_file)
+            X = dataset.iloc[:, :-1].values
+            y = dataset.iloc[:, -1].values
+            imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+            imputer.fit(X[:, :])
+            X[:, :] = imputer.transform(X[:, :])
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+            sc = StandardScaler()
+            X_train = sc.fit_transform(X_train)
+            X_test = sc.transform(X_test)
+            classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0)
+            classifier.fit(X_train, y_train)
+            result = classifier.predict(sc.transform(lst))
+            f_data = []
+            for res in result:
+                f_data.append(res)
+            context['result'] = f_data
+        except Exception as e:
+            return HttpResponse("Error Occured , Reason : " + str(e))
     return render(request,template_name,context)
 
 
@@ -235,22 +254,25 @@ def SupportVector(request):
         if not input_csv_file.name.endswith('.csv'):
             messages.error(request,'input file is not a valid CSV file')
             return redirect('support-vector')
-        dataset = pd.read_csv(csv_file)
-        lst = pd.read_csv(input_csv_file)
-        X = dataset.iloc[:, :-1].values
-        y = dataset.iloc[:, -1].values
-        imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
-        imputer.fit(X[:, :])
-        X[:, :] = imputer.transform(X[:, :])
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
-        sc = StandardScaler()
-        X_train = sc.fit_transform(X_train)
-        X_test = sc.transform(X_test)
-        classifier = SVC(kernel = 'linear', random_state = 0)
-        classifier.fit(X_train, y_train)
-        result = classifier.predict(sc.transform(lst))
-        f_data = []
-        for res in result:
-            f_data.append(res)
-        context['result'] = f_data
+        try:
+            dataset = pd.read_csv(csv_file)
+            lst = pd.read_csv(input_csv_file)
+            X = dataset.iloc[:, :-1].values
+            y = dataset.iloc[:, -1].values
+            imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+            imputer.fit(X[:, :])
+            X[:, :] = imputer.transform(X[:, :])
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+            sc = StandardScaler()
+            X_train = sc.fit_transform(X_train)
+            X_test = sc.transform(X_test)
+            classifier = SVC(kernel = 'linear', random_state = 0)
+            classifier.fit(X_train, y_train)
+            result = classifier.predict(sc.transform(lst))
+            f_data = []
+            for res in result:
+                f_data.append(res)
+            context['result'] = f_data
+        except Exception as e:
+            return HttpResponse("Error Occured , Reason : " + str(e))
     return render(request,template_name,context)
--- regression/views.py
+++ regression/views.py
@@ -27,20 +27,23 @@ def DecisonTreeRegression(request):
         if not input_csv_file.name.endswith('.csv'):
             messages.error(request, 'input file is not a valid CSV file')
             return redirect('decision-tree-regression')
-        dataset = pd.read_csv(csv_file)
-        lst = pd.read_csv(input_csv_file)
-        X = dataset.iloc[:, :-1].values
-        y = dataset.iloc[:, -1].values
-        imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
-        imputer.fit(X[:, :])
-        X[:, :] = imputer.transform(X[:, :])
-        regressor = DecisionTreeRegressor(random_state=0)
-        regressor.fit(X, y)
-        result = regressor.predict(lst)
-        f_data = []
-        for res in result:
-            f_data.append(res)
-        context['result'] = f_data
+        try:
+            dataset = pd.read_csv(csv_file)
+            lst = pd.read_csv(input_csv_file)
+            X = dataset.iloc[:, :-1].values
+            y = dataset.iloc[:, -1].values
+            imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+            imputer.fit(X[:, :])
+            X[:, :] = imputer.transform(X[:, :])
+            regressor = DecisionTreeRegressor(random_state=0)
+            regressor.fit(X, y)
+            result = regressor.predict(lst)
+            f_data = []
+            for res in result:
+                f_data.append(res)
+            context['result'] = f_data
+        except Exception as e:
+            return HttpResponse("Error Occured , Reason : " + str(e))
     return render(request, template_name, context)
 
 
@@ -58,22 +61,25 @@ def MultipleLinearRegression(request):
         if not input_csv_file.name.endswith('.csv'):
             messages.error(request, 'input file is not a valid CSV file')
             return redirect('multiple-linear-regression')
-        dataset = pd.read_csv(csv_file)
-        lst = pd.read_csv(input_csv_file)
-        X = dataset.iloc[:, :-1].values
-        y = dataset.iloc[:, -1].values
-        imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
-        imputer.fit(X[:, :])
-        X[:, :] = imputer.transform(X[:, :])
-        X_train, X_test, y_train, y_test = train_test_split(
-            X, y, test_size=0.2, random_state=0)
-        regressor = LinearRegression()
-        regressor.fit(X_train, y_train)
-        result = regressor.predict(lst)
-        f_data = []
-        for res in result:
-            f_data.append(res)
-        context['result'] = f_data
+        try:
+            dataset = pd.read_csv(csv_file)
+            lst = pd.read_csv(input_csv_file)
+            X = dataset.iloc[:, :-1].values
+            y = dataset.iloc[:, -1].values
+            imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+            imputer.fit(X[:, :])
+            X[:, :] = imputer.transform(X[:, :])
+            X_train, X_test, y_train, y_test = train_test_split(
+                X, y, test_size=0.2, random_state=0)
+            regressor = LinearRegression()
+            regressor.fit(X_train, y_train)
+            result = regressor.predict(lst)
+            f_data = []
+            for res in result:
+                f_data.append(res)
+            context['result'] = f_data
+        except Exception as e:
+            return HttpResponse("Error Occured , Reason : " + str(e))
     return render(request, template_name, context)
 
 
@@ -91,24 +97,27 @@ def PolynomialRegression(request):
         if not input_csv_file.name.endswith('.csv'):
             messages.error(request, 'input file is not a valid CSV file')
             return redirect('polynomial-regression')
-        dataset = pd.read_csv(csv_file)
-        lst = pd.read_csv(input_csv_file)
-        X = dataset.iloc[:, :-1].values
-        y = dataset.iloc[:, -1].values
-        imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
-        imputer.fit(X[:, :])
-        X[:, :] = imputer.transform(X[:, :])
-        lin_reg = LinearRegression()
-        lin_reg.fit(X, y)
-        poly_reg = PolynomialFeatures(degree=4)
-        X_poly = poly_reg.fit_transform(X)
-        lin_reg_2 = LinearRegression()
-        lin_reg_2.fit(X_poly, y)
-        result = lin_reg_2.predict(poly_reg.fit_transform(lst))
-        f_data = []
-        for res in result:
-            f_data.append(res)
-        context['result'] = f_data
+        try:
+            dataset = pd.read_csv(csv_file)
+            lst = pd.read_csv(input_csv_file)
+            X = dataset.iloc[:, :-1].values
+            y = dataset.iloc[:, -1].values
+            imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+            imputer.fit(X[:, :])
+            X[:, :] = imputer.transform(X[:, :])
+            lin_reg = LinearRegression()
+            lin_reg.fit(X, y)
+            poly_reg = PolynomialFeatures(degree=4)
+            X_poly = poly_reg.fit_transform(X)
+            lin_reg_2 = LinearRegression()
+            lin_reg_2.fit(X_poly, y)
+            result = lin_reg_2.predict(poly_reg.fit_transform(lst))
+            f_data = []
+            for res in result:
+                f_data.append(res)
+            context['result'] = f_data
+        except Exception as e:
+            return HttpResponse("Error Occured , Reason : " + str(e))
     return render(request, template_name, context)
 
 
@@ -126,20 +135,23 @@ def RandomForestRegression(request):
         if not input_csv_file.name.endswith('.csv'):
             messages.error(request, 'input file is not a valid CSV file')
             return redirect('random-forest-regression')
-        dataset = pd.read_csv(csv_file)
-        lst = pd.read_csv(input_csv_file)
-        X = dataset.iloc[:, :-1].values
-        y = dataset.iloc[:, -1].values
-        imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
-        imputer.fit(X[:, :])
-        X[:, :] = imputer.transform(X[:, :])
-        regressor = RandomForestRegressor(n_estimators=10, random_state=0)
-        regressor.fit(X, y)
-        result = regressor.predict(lst)
-        f_data = []
-        for res in result:
-            f_data.append(res)
-        context['result'] = f_data
+        try:
+            dataset = pd.read_csv(csv_file)
+            lst = pd.read_csv(input_csv_file)
+            X = dataset.iloc[:, :-1].values
+            y = dataset.iloc[:, -1].values
+            imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+            imputer.fit(X[:, :])
+            X[:, :] = imputer.transform(X[:, :])
+            regressor = RandomForestRegressor(n_estimators=10, random_state=0)
+            regressor.fit(X, y)
+            result = regressor.predict(lst)
+            f_data = []
+            for res in result:
+                f_data.append(res)
+            context['result'] = f_data
+        except Exception as e:
+            return HttpResponse("Error Occured , Reason : " + str(e))
     return render(request, template_name, context)
 
 
@@ -157,22 +169,25 @@ def SimpleLinearRegression(request):
         if not input_csv_file.name.endswith('.csv'):
             messages.error(request, 'input file is not a valid CSV file')
             return redirect('simple-linear-regression')
-        dataset = pd.read_csv(csv_file)
-        lst = pd.read_csv(input_csv_file)
-        X = dataset.iloc[:, :-1].values
-        y = dataset.iloc[:, -1].values
-        imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
-        imputer.fit(X[:, :])
-        X[:, :] = imputer.transform(X[:, :])
-        X_train, X_test, y_train, y_test = train_test_split(
-            X, y, test_size=1/3, random_state=0)
-        regressor = LinearRegression()
-        regressor.fit(X_train, y_train)
-        result = regressor.predict(lst)
-        f_data = []
-        for res in result:
-            f_data.append(res)
-        context['result'] = f_data
+        try:
+            dataset = pd.read_csv(csv_file)
+            lst = pd.read_csv(input_csv_file)
+            X = dataset.iloc[:, :-1].values
+            y = dataset.iloc[:, -1].values
+            imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+            imputer.fit(X[:, :])
+            X[:, :] = imputer.transform(X[:, :])
+            X_train, X_test, y_train, y_test = train_test_split(
+                X, y, test_size=1/3, random_state=0)
+            regressor = LinearRegression()
+            regressor.fit(X_train, y_train)
+            result = regressor.predict(lst)
+            f_data = []
+            for res in result:
+                f_data.append(res)
+            context['result'] = f_data
+        except Exception as e:
+            return HttpResponse("Error Occured , Reason : " + str(e))
     return render(request, template_name, context)
 
 
@@ -190,23 +205,26 @@ def SupportVectorRegression(request):
         if not input_csv_file.name.endswith('.csv'):
             messages.error(request, 'input file is not a valid CSV file')
             return redirect('support-vector-regression')
-        dataset = pd.read_csv(csv_file)
-        lst = pd.read_csv(input_csv_file)
-        X = dataset.iloc[:, :-1].values
-        y = dataset.iloc[:, -1].values
-        y = y.reshape(len(y), 1)
-        imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
-        imputer.fit(X[:, :])
-        X[:, :] = imputer.transform(X[:, :])
-        sc_X = StandardScaler()
-        sc_y = StandardScaler()
-        X = sc_X.fit_transform(X)
-        y = sc_y.fit_transform(y)
-        regressor = SVR(kernel='rbf')
-        regressor.fit(X, y.ravel())
-        result = sc_y.inverse_transform(regressor.predict(sc_X.transform(lst)))
-        f_data = []
-        for res in result:
-            f_data.append(res)
-        context['result'] = f_data
+        try:
+            dataset = pd.read_csv(csv_file)
+            lst = pd.read_csv(input_csv_file)
+            X = dataset.iloc[:, :-1].values
+            y = dataset.iloc[:, -1].values
+            y = y.reshape(len(y), 1)
+            imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+            imputer.fit(X[:, :])
+            X[:, :] = imputer.transform(X[:, :])
+            sc_X = StandardScaler()
+            sc_y = StandardScaler()
+            X = sc_X.fit_transform(X)
+            y = sc_y.fit_transform(y)
+            regressor = SVR(kernel='rbf')
+            regressor.fit(X, y.ravel())
+            result = sc_y.inverse_transform(regressor.predict(sc_X.transform(lst)))
+            f_data = []
+            for res in result:
+                f_data.append(res)
+            context['result'] = f_data
+        except Exception as e:
+            return HttpResponse("Error Occured , Reason : " + str(e))
     return render(request, template_name, context)
