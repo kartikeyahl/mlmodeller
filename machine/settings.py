da361cbdacb9294a8581d280c467284e87fb12e0
developerbuilds <prince.malethia.36@gmail.com>
2020-12-17 14:55:29 +0530

    settings update

--- machine/settings.py
+++ machine/settings.py
@@ -14,7 +14,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'classification',
-    'deeplearning',
+    #'deeplearning',
     'regression',
 ]
 MIDDLEWARE = [
